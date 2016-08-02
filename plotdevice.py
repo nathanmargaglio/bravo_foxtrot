33d0e3224621e510ede4c88a91ce833190eb1bc0
nathanmargaglio <nathanmargaglio@gmail.com>
2016-08-02 16:56:52 -0400

    Fixed a line.

--- plotdevice.py
+++ plotdevice.py
@@ -1,5 +1,5 @@
 import matplotlib
-#~ matplotlib.use('Agg') # delete this for live plot, need it for SSH
+matplotlib.use('Agg') # omit this for live plot, need it for SSH
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
 from matplotlib import text
