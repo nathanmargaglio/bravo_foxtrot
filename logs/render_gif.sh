8e334a3d91e5adb836fa6e5ddea0bc9dc3d644d7
nathanmargaglio <nathanmargaglio@gmail.com>
2016-08-03 00:01:09 -0400

    Fixed hard-coded dir

--- logs/render_gif.sh
+++ logs/render_gif.sh
@@ -2,5 +2,5 @@
 if [ ! -d "$gifs" ]; then
         mkdir gifs
 fi
-ffmpeg -y -i ~/everything/logs/"$1"/img/%d.png -vf palettegen ~/everything/logs/"$1"/img/palette.png
-ffmpeg -y -framerate 6 -i ~/everything/logs/"$1"/img/%d.png -i ~/everything/logs/"$1"/img/palette.png -lavfi paletteuse ~/everything/logs/gifs/"$1$2".gif
+ffmpeg -y -i "$1"/img/%d.png -vf palettegen "$1"/img/palette.png
+ffmpeg -y -framerate 6 -i "$1"/img/%d.png -i "$1"/img/palette.png -lavfi paletteuse gifs/"$1$2".gif
