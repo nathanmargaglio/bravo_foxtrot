522083db606db72b2678a8fac4628e5fedc032ca
nathanmargaglio <nathanmargaglio@gmail.com>
2016-08-29 11:10:30 -0400

    Expired_listings

---
+++ expired_listings/server.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+import pandas as pd
+import os
+
+SHOW_COLS = ['ML #', 'Address', 'City', 'Zip Code_x', 'Owner 1 First Name',
+				'Owner 1 Last Name', 'List Date', 'DOM', 'Phone']
+
+def get_dir_list():
+	dir_list = []
+	for i in os.listdir('data'):
+		try:
+			f = open("data/{}/results.csv".format(i))
+			dir_list.append(i)
+		except:
+			pass
+	return dir_list
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	dir_list = get_dir_list()
+	return render_template("index.html", dl=dir_list)
+	
+@app.route("/<exp>")
+def exp(exp):
+	data = pd.read_csv("data/"+exp+"/results.csv")
+	return render_template("expires.html", data=data, cols=SHOW_COLS)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=80,debug=True)
